f160293b0eefb21a14d990818bd64cc6a0b1e0b7
iosubdragos <iosubdragosc@gmail.com>
2025-12-30 09:39:07 +0000

    added banner at start

--- style.sh
+++ style.sh
@@ -12,3 +12,24 @@ show_prompt() {
     echo -ne "${GREEN}amsh${RESET}:${BLUE}${current_dir}${RESET}> "
 }
 
+show_welcome_banner() {
+    local CYAN='\033[1;36m'
+    local GREEN='\033[1;32m'
+    local YELLOW='\033[1;33m'
+    local RESET='\033[0m'
+
+    clear
+
+    echo -e "${CYAN}==================================================${RESET}"
+    echo -e "${CYAN}|${RESET}                                                ${CYAN}|${RESET}"
+    echo -e "${CYAN}|${RESET}      ${GREEN}Auto Mount Shell ${RESET}              ${CYAN}|${RESET}"
+    echo -e "${CYAN}|${RESET}                            ITBI -  Proiect 2025         ${CYAN}|${RESET}"
+    echo -e "${CYAN}|${RESET}                   By Ababei Raul  & Iosub Dragos        ${CYAN}|${RESET}"
+    echo -e "${CYAN}|${RESET}                                                                                        ${CYAN}|${RESET}"
+    echo -e "${CYAN}==================================================${RESET}"
+    
+    echo -e "User: ${YELLOW}$(whoami)${RESET} | Host: ${YELLOW}$(hostname)${RESET} | Data: ${YELLOW}$(date +'%Y-%m-%d %H:%M')${RESET}"
+    echo -e "Configuratie incarcata din: ${YELLOW}amsh.conf${RESET}"
+    echo -e "--------------------------------------------------"
+    echo ""
+}
